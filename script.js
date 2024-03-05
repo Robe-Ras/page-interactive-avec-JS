095d9be5f1349ead13c6fbc50b0f75da5b8fa3c6
Ton nom d'utilisateur github <Ton adresse mail github>
2024-03-05 08:52:12 +0100

    last commit

--- script.js
+++ script.js
@@ -67,11 +67,97 @@ body.addEventListener('dblclick', function(bodyClick) {
 let reduceHover20 = document.querySelectorAll('.btn.btn-sm.btn-success');
 
 reduceHover20.forEach(function(button) {
+  let isReduced = false; 
+
   button.addEventListener('mouseover', function() {
-    let card = button.parentElement.parentElement.parentElement.parentElement;
+    let card = button.closest('.card');
     let imgToReduce = card.querySelector('.card-img-top'); 
     let textToHide = card.querySelector('.card-text');
-    imgToReduce.style.transform = 'scale(20%)';
-    textToHide.style.display = 'none';
+
+    if (!isReduced) { 
+      imgToReduce.style.transform = 'scale(20%)';
+      textToHide.style.display = 'none';
+      isReduced = true; 
+
+    } else { 
+      imgToReduce.style.transform = '';
+      textToHide.style.display = '';
+      isReduced = false; 
+    }
   });
 });
+
+
+
+
+// Fonctionnalité 7 :
+let buttonNext = document.querySelector('.btn.btn-secondary.my-2');
+
+buttonNext.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  let lastCard = document.querySelector('.col-md-4:last-child');
+  let cardContainer = document.querySelector('.album .row');
+
+  cardContainer.prepend(lastCard);
+});
+
+
+
+
+//Fonctionnalité 8 :
+let buttonPrevious = document.querySelector('.btn.btn-primary.my-2');
+
+buttonPrevious.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  let firstCard = document.querySelector('.col-md-4:first-child');
+  let cardContainer = document.querySelector('.album .row');
+
+  cardContainer.append(firstCard);
+});
+
+
+
+//Fonctionnalité 9 :
+let logo = document.querySelector('.navbar-brand');
+
+function highlightLogo() {
+  logo.classList.add('highlighted');
+}
+
+function unhighlightLogo() {
+  logo.classList.remove('highlighted');
+}
+
+
+logo.addEventListener('mouseover', highlightLogo);
+logo.addEventListener('mouseout', unhighlightLogo);
+
+
+document.addEventListener('keypress', function(event) {
+  if (logo.classList.contains('highlighted')) {
+    
+    let key = event.key;
+    let body = document.body;
+    body.className = '';  // pour supprimer toutes les classes du body
+
+    
+    if (key === 'a') {
+      body.classList.add('col-4');
+    }
+    
+    else if (key === 'y') {
+      body.classList.add('col-4', 'offset-md-4');
+    }
+   
+    else if (key === 'p') {
+      body.classList.add('col-4', 'offset-md-8');
+    }
+    
+    else {
+    
+    }
+  }
+});
+
